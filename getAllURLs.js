e19408e604ba34a456a85cd95c0e29c989ae7c0c
C4illin <emrik.ostling@gmail.com>
2021-05-08 20:15:15 +0200

    updated

--- getAllURLs.js
+++ getAllURLs.js
@@ -13,50 +13,65 @@ const fs = require("fs");
   // await page.setDefaultNavigationTimeout(0)
  
 
-  let url = "https://www.systembolaget.se/sok/?newArrivalType=Nytt%20senaste%20m%C3%A5naden" + "&page="
+  // let url = "https://www.systembolaget.se/sok/?newArrivalType=Nytt%20senaste%20m%C3%A5naden" + "&page="
   // let url = "https://www.systembolaget.se/sok/?" + "&page="
 
+  // products:
+  // 5062
+  // 5042
+  // 5356
+  // 6299
+  let urlArray = [
+    "https://www.systembolaget.se/sok/?priceFrom=400",
+    "https://www.systembolaget.se/sok/?priceFrom=200&priceTo=400",
+    "https://www.systembolaget.se/sok/?priceFrom=100&priceTo=200",
+    "https://www.systembolaget.se/sok/?priceTo=100"
+  ]
+
   // there is a cap at 666 pages (9990 products) but enough products for 1460 pages (21897 prudcts @ 15 per page)
-
   let productCounter = 0
-  let pageCounter = 1
-  while (pageCounter > 0) {
-    await page.goto(url + pageCounter)
-    await page.waitForSelector(".col-12.col-lg-9 > div > div:nth-child(2) > div").then(() => {
-      console.log(pageCounter)
-    }).catch(() => {
-      console.log('FAIL' + pageCounter)
-      page.screenshot({ path: 'testfail.png' })
-    })
+  for(let urlindex = 0; urlindex < urlArray.length; urlindex++) {
+    let url = urlArray[urlindex] + "&page="
+    let pageCounter = 1
+    while (pageCounter > 0) {
+      await page.goto(url + pageCounter)
+      await page.waitForSelector(".col-12.col-lg-9 > div > div:nth-child(2) > div").then(() => {
+        console.log(pageCounter)
+      }).catch(() => {
+        console.log('FAIL' + pageCounter)
+        page.screenshot({ path: 'testfail.png' })
+      })
     
-    // hrefs is unnessesary middle step
-    let hrefs = await page.$$eval('a', as => as.map(a => (a.href)))
-    let hrefsFiltered = hrefs.filter((link) => link.startsWith("https://www.systembolaget.se/produkt/"))
+      // hrefs is unnessesary middle step
+      let hrefs = await page.$$eval('a', as => as.map(a => (a.href)))
+      let hrefsFiltered = hrefs.filter((link) => link.startsWith("https://www.systembolaget.se/produkt/"))
 
-    let counter = 0
-    if (hrefsFiltered.length == 0) {
-      await page.screenshot({ path: 'test.png' })
-      pageCounter = 0
-    } else {
-      for (let i = 0; i < hrefsFiltered.length; i++) {
-        productCounter += 1
-        if (!urls.includes(hrefsFiltered[i])) {
-          urls.push(hrefsFiltered[i])
-          counter += 1
+      let counter = 0
+      if (hrefsFiltered.length == 0) {
+        await page.screenshot({ path: 'test.png' })
+        pageCounter = 0
+      } else {
+        for (let i = 0; i < hrefsFiltered.length; i++) {
+          productCounter += 1
+          if (!urls.includes(hrefsFiltered[i])) {
+            urls.push(hrefsFiltered[i])
+            counter += 1
+          }
         }
+        pageCounter += 1
       }
-      pageCounter += 1
+      console.log("Found: " + counter)
     }
-    console.log("Found: " + counter)
+
+    console.log("Pages: " + pageCounter)
+    console.log("Total: " + productCounter)
+    console.log("Slut storlek: " + urls.length)
+    console.log("Delta: " + (urls.length - startSize))
+
+    let file = fs.createWriteStream('urls.txt')
+    file.write(urls.join('\n'))
+    file.end()
   }
-
-  console.log("Total: " + productCounter)
-  console.log("Slut storlek: " + urls.length)
-  console.log("Delta: " + (urls.length - startSize))
-
-  let file = fs.createWriteStream('urls.txt')
-  file.write(urls.join('\n'))
-  file.end()
-
+  
   await browser.close()
 })()
--- getProductInfo.js
+++ getProductInfo.js
@@ -12,7 +12,7 @@ const fs = require("fs");
   await page.click('button[type="secondary"]')
   await page.screenshot({ path: 'test.png' })
 
-  for (let i = 20690; i < urls.length; i++) { //urls.length
+  for (let i = 21450; i < urls.length; i++) { //urls.length
     console.log(i + 1 + " - " + urls[i])
     await page.goto(urls[i])
 
