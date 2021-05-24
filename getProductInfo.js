a532c642cda405519023e5181138a9eba41b1e1f
C4illin <emrik.ostling@gmail.com>
2021-05-24 20:52:57 +0200

    fix sugar

--- getProductInfo.js
+++ getProductInfo.js
@@ -12,7 +12,7 @@ const fs = require("fs");
   await page.click('button[type="secondary"]')
   await page.screenshot({ path: 'test.png' })
 
-  for (let i = 0; i < urls.length; i++) { //urls.length
+  for (let i = 21000; i < urls.length; i++) { //urls.length
     console.log(i + 1 + " - " + urls[i])
     await page.goto(urls[i])
 
@@ -40,6 +40,7 @@ const fs = require("fs");
         }
 
         if (product["alcohol"]?.endsWith("g/l")) {
+          product["sugar"] = product["alcohol"]
           product["alcohol"] = "0 %"
         }
 
