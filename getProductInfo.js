d72f8483dcd7c374f304f3f8014f1d07abd059eb
C4illin <emrik.ostling@gmail.com>
2021-05-26 20:29:43 +0200

    updated

--- getProductInfo.js
+++ getProductInfo.js
@@ -12,7 +12,7 @@ const fs = require("fs");
   await page.click('button[type="secondary"]')
   await page.screenshot({ path: 'test.png' })
 
-  for (let i = 22000; i < urls.length; i++) { //urls.length
+  for (let i = 22300; i < urls.length; i++) { //urls.length
     console.log(i + 1 + " - " + urls[i])
     await page.goto(urls[i])
 
