6ecd376f64db58159f443a612c15fbfd8f62e9f0
C4illin <emrik.ostling@gmail.com>
2024-03-09 00:35:02 +0100

    2023 added

--- excel/excel.js
+++ excel/excel.js
@@ -1,118 +1,122 @@
 const readXlsxFile = require("read-excel-file/node");
-const fs = require("fs");
+const fs = require("node:fs");
 
 const fileNameList = [
-  "Artikellistan 2022.xlsx",
-  "Artikellistan 2021.xlsx",
-  "Artikellistan 2020.xlsx",
-  "Artikellistan 2019.xlsx",
-  "2018_Artikellistan_Ny.xlsx",
-  "2017_Artikellistan_Ny.xlsx",
-  "2016_Artikellistan.xlsx",
-  "2015-Forsaljning-per-artikel_2.xlsx",
-  "2014-Forsaljning-per-artikel_.xlsx",
-  "2013-Forsaljning-per-artikel_.xlsx",
-  "2012-Forsaljning-per-artikel_.xlsx",
-  "2011-Forsaljning-per-artikel_.xlsx",
-  "2010-Forsaljning-per-artikel_.xlsx",
-  "2009-Forsaljning-per-artikel_.xlsx",
+	"Artikellistan 2023.xlsx",
+	"Artikellistan 2022.xlsx",
+	"Artikellistan 2021.xlsx",
+	"Artikellistan 2020.xlsx",
+	"Artikellistan 2019.xlsx",
+	"2018_Artikellistan_Ny.xlsx",
+	"2017_Artikellistan_Ny.xlsx",
+	"2016_Artikellistan.xlsx",
+	"2015-Forsaljning-per-artikel_2.xlsx",
+	"2014-Forsaljning-per-artikel_.xlsx",
+	"2013-Forsaljning-per-artikel_.xlsx",
+	"2012-Forsaljning-per-artikel_.xlsx",
+	"2011-Forsaljning-per-artikel_.xlsx",
+	"2010-Forsaljning-per-artikel_.xlsx",
+	"2009-Forsaljning-per-artikel_.xlsx",
 ];
 
 let products = [];
-let item = 0
+const item = 0;
 
 fs.readFile("../data/products.json", function read(err, data) {
-  if (!err && data) {
-    console.log("Products.json loaded");
-    products = JSON.parse(data);
-    processItem(item)
-  } else if (err.code == "ENOENT") {
-    console.log("Products.json not found, generate it or download it");
-  } else {
-    console.log("Error with products.json: ", err.code);
-  }
+	if (!err && data) {
+		console.log("Products.json loaded");
+		products = JSON.parse(data);
+		processItem(item);
+	} else if (err.code === "ENOENT") {
+		console.log("Products.json not found, generate it or download it");
+	} else {
+		console.log("Error with products.json: ", err.code);
+	}
 });
 
-let processItem = (item) => {
-  if (item < fileNameList.length) {
+const processItem = (item) => {
+	if (item < fileNameList.length) {
+		const fileName = fileNameList[item];
+		console.log("Processing:", fileName);
+		const newInfoMap = {};
 
-    let fileName = fileNameList[item];
-    console.log("Processing:", fileName);
-    let newInfoMap = {};
+		const year = fileName
+			.replace("_2.xls", "")
+			.replaceAll("-", " ")
+			.replaceAll("_", " ")
+			.replaceAll(".", " ")
+			.replaceAll("  ", " ")
+			.split(" ")
+			.sort()[0];
 
-    let year = fileName
-      .replace("_2.xls", "")
-      .replaceAll("-", " ")
-      .replaceAll("_", " ")
-      .replaceAll(".", " ")
-      .replaceAll("  ", " ")
-      .split(" ")
-      .sort()[0];
+		const changedDate = new Date(Number.parseInt(year) + 1, 0, 1).valueOf();
 
-    let changedDate = new Date(parseInt(year) + 1, 0, 1).valueOf();
-    
-    console.log("Year:", year, "Epoch:", changedDate);
+		console.log("Year:", year, "Epoch:", changedDate);
 
-    let artNrIndex = 0;
+		let artNrIndex = 0;
 
-    if (year < 2013) {
-      artNrIndex = 1;
-    }
+		if (year < 2013) {
+			artNrIndex = 1;
+		}
 
-    readXlsxFile(fileName)
-      .then((rows) => {
-        for (const row of rows) {
-          let artNr = row[artNrIndex];
-          let price = row[8];
-          let sold = row[16];
-          newInfoMap[artNr] = { price: price, sold: sold };
-        }
-      })
-      .finally(() => {
-        // if (testing) {
-        //   fs.writeFile(
-        //     "newInfo.json",
-        //     JSON.stringify(newInfoMap, null, 2),
-        //     (err) => {
-        //       if (err) throw err;
-        //       console.log("Check newInfo.json!");
-        //     }
-        //   );
-        // } else {
-        for (const product of products) {
-          if (newInfoMap[product.productNumber]) {
-            let toAddPrice = {
-              x: changedDate,
-              y: newInfoMap[product.productNumber].price,
-            };
-            let toAddSold = {
-              x: changedDate,
-              y: newInfoMap[product.productNumber].sold,
-            };
+		readXlsxFile(fileName)
+			.then((rows) => {
+				for (const row of rows) {
+					const artNr = row[artNrIndex];
+					const price = row[8];
+					const sold = row[16];
+					newInfoMap[artNr] = { price: price, sold: sold };
+				}
+			})
+			.finally(() => {
+				// if (testing) {
+				//   fs.writeFile(
+				//     "newInfo.json",
+				//     JSON.stringify(newInfoMap, null, 2),
+				//     (err) => {
+				//       if (err) throw err;
+				//       console.log("Check newInfo.json!");
+				//     }
+				//   );
+				// } else {
+				for (const product of products) {
+					if (newInfoMap[product.productNumber]) {
+						const toAddPrice = {
+							x: changedDate,
+							y: newInfoMap[product.productNumber].price,
+						};
+						const toAddSold = {
+							x: changedDate,
+							y: newInfoMap[product.productNumber].sold,
+						};
 
-            if (product.priceHistory[0].x != changedDate) {
-              product.priceHistory.unshift(toAddPrice);
-            }
+						if (product.priceHistory[0].x !== changedDate) {
+							product.priceHistory.unshift(toAddPrice);
+						}
 
-            if (product.soldVolume) {
-              if (product.soldVolume[0].x != changedDate) {
-                product.soldVolume.unshift(toAddSold);
-              }
-            } else {
-              product.soldVolume = [toAddSold];
-            }
-          }
-        }
-        processItem(item + 1)
-      });
-  } else {
-    fs.writeFile(
-      "../data/products.json",
-      JSON.stringify(products, null, 2),
-      (err) => {
-        if (err) throw err;
-        console.log("Saved new products.json!");
-      }
-    );
-  }
+						if (product.soldVolume) {
+							if (product.soldVolume[0].x !== changedDate) {
+								product.soldVolume.unshift(toAddSold);
+							}
+						} else {
+							product.soldVolume = [toAddSold];
+						}
+
+						// Sort priceHistory and soldVolume in ascending order
+						product.priceHistory.sort((a, b) => a.x - b.x);
+						product.soldVolume.sort((a, b) => a.x - b.x);
+					}
+				}
+				processItem(item + 1);
+			});
+	} else {
+		fs.writeFile(
+			"../data/products.json",
+			JSON.stringify(products, null, 2),
+			(err) => {
+				if (err) throw err;
+				console.log("Saved new products.json!");
+			},
+		);
+	}
 };
--- index.js
+++ index.js
@@ -1,79 +1,82 @@
-import express from 'express';
-import { CronJob } from 'cron';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-import { getAllProducts } from './getAllProducts.js';
-import compression from "compression"
-import helmet from "helmet"
-import cors from "cors"
-import { readFile } from 'fs';
+import express from "express";
+import { CronJob } from "cron";
+import { fileURLToPath } from "node:url";
+import { dirname } from "node:path";
+import { getAllProducts } from "./getAllProducts.js";
+import compression from "compression";
+import helmet from "helmet";
+import cors from "cors";
+import { readFile } from "node:fs";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-let productIdMap = {};
+const productIdMap = {};
 
 const idMap = (products) => {
-  for (const product of products) {
-    productIdMap[product.productNumber] = product
-  }
-  console.log("IdMap created")
-}
+	for (const product of products) {
+		productIdMap[product.productNumber] = product;
+	}
+	console.log("IdMap created");
+};
 
 readFile("data/products.json", function read(err, data) {
-  if(!err && data) {
-    console.log("Products.json found, no need to fetch")
-    idMap(JSON.parse(data))
-  } else if(err.code == "ENOENT") {
-    console.log("Products.json not found, this takes a while...");
-    getAllProducts(productIdMap).then((products) => {
-      idMap(products)
-    })
-  } else {
-    console.log("Error with products.json: ", err.code)
-  }
-})
+	if (!err && data) {
+		console.log("Products.json found, no need to fetch");
+		idMap(JSON.parse(data));
+	} else if (err.code === "ENOENT") {
+		console.log("Products.json not found, this takes a while...");
+		getAllProducts(productIdMap).then((products) => {
+			idMap(products);
+		});
+	} else {
+		console.log("Error with products.json: ", err.code);
+	}
+});
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(compression())
-app.use(helmet())
-app.use(cors())
+app.use(compression());
+app.use(helmet());
+app.use(cors());
 
-app.get('/v1/products', (_req, res) => {
-  res.sendFile(__dirname + '/data/products.json');
+app.get("/v1/products", (_req, res) => {
+	res.sendFile(`${__dirname}/data/products.json`);
 });
 
-app.get('/v1/products/updated', (req, res) => {
-  res.sendFile(__dirname + '/data/updated.json');
+app.get("/v1/products/updated", (_req, res) => {
+	res.sendFile(`${__dirname}/data/updated.json`);
 });
 
 app.get("/v1/product/:id", (req, res) => {
-  const result = productIdMap[req.params.id]
-  if (result) {
-    res.json(result)
-  } else {
-    res.status(404).json({"error": "404, product not found"})
-  }
-  // res.json(productIdMap[req.params.id] || { "error": "Product not found"})
+	const result = productIdMap[req.params.id];
+	if (result) {
+		res.json(result);
+	} else {
+		res.status(404).json({ error: "404, product not found" });
+	}
+	// res.json(productIdMap[req.params.id] || { "error": "Product not found"})
 });
 
-app.get('/', (_req, res) => {
-  res.redirect('v1/products');
-})
+app.get("/", (_req, res) => {
+	res.redirect("v1/products");
+});
 
-app.get('*', function(req, res){
-  res.status(404).send("404, page not found. See <a href='https://github.com/C4illin/systembolaget-data'>https://github.com/C4illin/systembolaget-data</a> for documentation.");
+app.get("*", (_req, res) => {
+	res
+		.status(404)
+		.send(
+			"404, page not found. See <a href='https://github.com/C4illin/systembolaget-data'>https://github.com/C4illin/systembolaget-data</a> for documentation.",
+		);
 });
 
 app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}`);
+	console.log(`Listening at http://localhost:${port}`);
 });
 
-const updateProducts = new CronJob('0 3 * * *', () => {
-  console.log('Updating ALL products')
-  getAllProducts(productIdMap).then((products) => {
-    idMap(products)
-  })
-});
-updateProducts.start()
+new CronJob("0 0 3 * * *", () => {
+	console.log("Updating ALL products");
+	getAllProducts(productIdMap).then((products) => {
+		idMap(products);
+	});
+}, null, true, "Europe/Stockholm");
