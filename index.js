2b3236a34af3f073145877a2251570162ef7b3df
C4illin <emrik.ostling@gmail.com>
2024-03-12 12:13:32 +0100

    log ip of request

--- index.js
+++ index.js
@@ -40,7 +40,10 @@ app.use(compression());
 app.use(helmet());
 app.use(cors());
 
-app.get("/v1/products", (_req, res) => {
+app.get("/v1/products", (req, res) => {
+	// print who IP of the request
+	const ip = req.headers["x-forwarded-for"] || req.socket.remoteAddress; 
+	console.log(`Request from ${ip}`);
 	res.sendFile(`${__dirname}/data/products.json`);
 });
 
